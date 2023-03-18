1a8a4e317072286510ed8f90f448b83ba9ce5c44
Luca Filipozzi <luca.filipozzi@gmail.com>
2023-03-18 16:51:37 -0700

    reorganize packages

--- require-role-authenticator/src/main/java/com/github/lucafilipozzi/keycloak/authentication/authenticators/browser/RequireRoleAuthenticator.java
+++ require-role-authenticator/src/main/java/com/github/lucafilipozzi/keycloak/authentication/authenticators/browser/RequireRoleAuthenticator.java
@@ -1,6 +1,6 @@
 // Copyright 2023 Luca Filipozzi. Some rights reserved. See LICENSE.
 
-package com.github.lucafilipozzi.keycloak.authentication.authenticators;
+package com.github.lucafilipozzi.keycloak.authentication.authenticators.browser;
 
 import com.google.common.collect.Sets;
 import java.util.Map;
--- require-role-authenticator/src/main/java/com/github/lucafilipozzi/keycloak/authentication/authenticators/browser/RequireRoleAuthenticatorFactory.java
+++ require-role-authenticator/src/main/java/com/github/lucafilipozzi/keycloak/authentication/authenticators/browser/RequireRoleAuthenticatorFactory.java
@@ -1,6 +1,6 @@
 // Copyright 2023 Luca Filipozzi. Some rights reserved. See LICENSE.
 
-package com.github.lucafilipozzi.keycloak.authentication.authenticators;
+package com.github.lucafilipozzi.keycloak.authentication.authenticators.browser;
 
 import java.util.List;
 import org.jboss.logging.Logger;
