0e79d919de3bbb8d2230891e29a29011df7fd0ca
Luca Filipozzi <luca.filipozzi@gmail.com>
2023-03-24 07:52:00 -0700

    implemented UsernamePolicyAuthenticator

---
+++ remove-required-action-authenticator/src/main/java/com/github/lucafilipozzi/keycloak/authentication/authenticators/UsernamePolicyAuthenticator.java
@@ -0,0 +1,71 @@
+// Copyright 2023 Luca Filipozzi. Some rights reserved. See LICENSE.
+
+package com.github.lucafilipozzi.keycloak.authentication.authenticators;
+
+import static javax.ws.rs.core.Response.Status.INTERNAL_SERVER_ERROR;
+import static javax.ws.rs.core.Response.Status.NOT_ACCEPTABLE;
+import static org.keycloak.authentication.AuthenticationFlowError.INTERNAL_ERROR;
+import static org.keycloak.authentication.AuthenticationFlowError.INVALID_USER;
+import static org.keycloak.authentication.authenticators.broker.AbstractIdpAuthenticator.BROKERED_CONTEXT_NOTE;
+
+import javax.ws.rs.core.Response;
+import lombok.extern.jbosslog.JBossLog;
+import org.keycloak.authentication.AuthenticationFlowContext;
+import org.keycloak.authentication.Authenticator;
+import org.keycloak.authentication.authenticators.broker.util.SerializedBrokeredIdentityContext;
+import org.keycloak.models.AuthenticatorConfigModel;
+import org.keycloak.models.KeycloakSession;
+import org.keycloak.models.RealmModel;
+import org.keycloak.models.UserModel;
+import org.keycloak.sessions.AuthenticationSessionModel;
+
+@JBossLog
+public class UsernamePolicyAuthenticator implements Authenticator {
+  @Override
+  public void action(AuthenticationFlowContext context) {
+    // intentionally empty
+  }
+
+  @Override
+  public void authenticate(AuthenticationFlowContext context) {
+    AuthenticatorConfigModel authenticatorConfig = context.getAuthenticatorConfig();
+    if (authenticatorConfig == null) {
+      Response response = context.form().setError("Server Misconfiguration").createErrorPage(INTERNAL_SERVER_ERROR);
+      context.failureChallenge(INTERNAL_ERROR, response);
+      return;
+    }
+    String pattern = authenticatorConfig.getConfig().get("pattern");
+
+    AuthenticationSessionModel authSession = context.getAuthenticationSession();
+    SerializedBrokeredIdentityContext serializedCtx = SerializedBrokeredIdentityContext.readFromAuthenticationSession(authSession, BROKERED_CONTEXT_NOTE);
+    String username = serializedCtx.getUsername();
+
+    if (!username.matches(pattern)) {
+      Response response = context.form().setError("Invalid Username").createErrorPage(NOT_ACCEPTABLE);
+      context.failureChallenge(INVALID_USER, response);
+      return;
+    }
+
+    context.success();
+  }
+
+  @Override
+  public void close() {
+    // intentionally empty
+  }
+
+  @Override
+  public boolean configuredFor(KeycloakSession session, RealmModel realm, UserModel user) {
+    return false;
+  }
+
+  @Override
+  public boolean requiresUser() {
+    return false;
+  }
+
+  @Override
+  public void setRequiredActions(KeycloakSession session, RealmModel realm, UserModel user) {
+    // intentionally empty
+  }
+}
---
+++ remove-required-action-authenticator/src/main/java/com/github/lucafilipozzi/keycloak/authentication/authenticators/UsernamePolicyAuthenticatorFactory.java
@@ -0,0 +1,99 @@
+// Copyright 2023 Luca Filipozzi. Some rights reserved. See LICENSE.
+
+package com.github.lucafilipozzi.keycloak.authentication.authenticators;
+
+import static org.keycloak.models.AuthenticationExecutionModel.Requirement.DISABLED;
+import static org.keycloak.models.AuthenticationExecutionModel.Requirement.REQUIRED;
+
+import java.util.Collections;
+import java.util.List;
+import lombok.NonNull;
+import org.keycloak.Config.Scope;
+import org.keycloak.authentication.Authenticator;
+import org.keycloak.authentication.AuthenticatorFactory;
+import org.keycloak.models.AuthenticationExecutionModel.Requirement;
+import org.keycloak.models.KeycloakSession;
+import org.keycloak.models.KeycloakSessionFactory;
+import org.keycloak.provider.ProviderConfigProperty;
+import org.keycloak.provider.ProviderConfigurationBuilder;
+
+public class UsernamePolicyAuthenticatorFactory implements AuthenticatorFactory {
+  public static final String PROVIDER_ID = "username-policy-authenticator";
+
+  private static final Requirement[] REQUIREMENT_CHOICES = { REQUIRED, DISABLED };
+
+  private static final UsernamePolicyAuthenticator SINGLETON = new UsernamePolicyAuthenticator();
+
+  private static final List<ProviderConfigProperty> CONFIG_PROPERTIES;
+
+  static {
+    CONFIG_PROPERTIES = ProviderConfigurationBuilder.create()
+      .property()
+      .name("pattern")
+      .type(ProviderConfigProperty.STRING_TYPE)
+      .label("pattern")
+      .helpText("provide pattern to validate against")
+      .add()
+      .build();
+  }
+
+  @Override
+  public void close() {
+    // intentionally empty
+  }
+
+  @Override
+  public Authenticator create(@NonNull KeycloakSession session) {
+    return SINGLETON;
+  }
+
+  @Override
+  public List<ProviderConfigProperty> getConfigProperties() {
+    return CONFIG_PROPERTIES;
+  }
+
+  @Override
+  public String getDisplayType() {
+    return "username policy enforcement";
+  }
+
+  @Override
+  public String getHelpText() {
+    return "specify the patterns to enforce on incoming usernames";
+  }
+
+  @Override
+  public String getId() {
+    return PROVIDER_ID;
+  }
+
+  @Override
+  public String getReferenceCategory() {
+    return "override";
+  }
+
+  @Override
+  public Requirement[] getRequirementChoices() {
+    return REQUIREMENT_CHOICES;
+  }
+
+  @Override
+  public void init(Scope config) {
+    // intentionally empty
+  }
+
+  @Override
+  public boolean isConfigurable() {
+    return true;
+  }
+
+  @Override
+  public boolean isUserSetupAllowed() {
+    return false;
+  }
+
+  @Override
+  public void postInit(KeycloakSessionFactory factory) {
+    // intentionally empty
+  }
+}
