08e6c73bf65636558ac025e1608f51bf3cd2b0c1
Luca Filipozzi <luca.filipozzi@gmail.com>
2023-04-20 18:03:17 -0700

    improve tests

--- md5crypt-password-hash-provider/src/test/java/com/github/lucafilipozzi/keycloak/credential/hash/Md5CryptPasswordHashProviderTest.java
+++ md5crypt-password-hash-provider/src/test/java/com/github/lucafilipozzi/keycloak/credential/hash/Md5CryptPasswordHashProviderTest.java
@@ -1,38 +1,33 @@
+// Copyright 2023 Luca Filipozzi. Some rights reserved. See LICENSE.
+
 package com.github.lucafilipozzi.keycloak.credential.hash;
 
 import static com.github.lucafilipozzi.keycloak.credential.hash.Md5CryptPasswordHashProviderFactory.PROVIDER_ID;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.*;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
 
 import java.util.Arrays;
 import java.util.List;
+import lombok.RequiredArgsConstructor;
 import org.apache.commons.codec.digest.Md5Crypt;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runners.Parameterized;
 import org.junit.runners.Parameterized.Parameters;
 import org.junit.runner.RunWith;
 import org.keycloak.models.credential.PasswordCredentialModel;
 
+@RequiredArgsConstructor
 @RunWith(Parameterized.class)
 public class Md5CryptPasswordHashProviderTest {
-  private Md5CryptPasswordHashProvider provider;
+  private final Md5CryptPasswordHashProvider provider = new Md5CryptPasswordHashProvider();
   private final String password;
   private final String expected;
-
-  public Md5CryptPasswordHashProviderTest(String password, String expected) {
-    this.password = password;
-    this.expected = expected;
-  }
-
-  @Before
-  public void init() {
-    provider = new Md5CryptPasswordHashProvider();
-  }
 
   @Parameters
   public static List<Object[]> data() {
     return Arrays.asList(new Object[][] {
+        /* password, expected */
         { "testingonly$$1234", "$1$PhQy/mw.$dDp.eDLeG6H0gz.WlhNV./" },
         { "Hello$$1234", "$1$v8ZrPcRS$mmBfzNIgRLnYO6jL3mWhr/" },
         { "Wong$$5678", "$1$gr8/T6QQ$Yq/2P3RlGoTHIFmQJ.q1S/" }
