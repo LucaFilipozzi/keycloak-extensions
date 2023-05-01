4d956eb07de0247ffcdd406792cf302d05a69cc4
Luca Filipozzi <luca.filipozzi@gmail.com>
2023-05-01 09:12:37 -0700

    introduce custom-password-policy-provider

---
+++ custom-password-policy-provider/README.md
@@ -0,0 +1,14 @@
+# custom-password-policy-provider
+
+## description
+
+TODO
+
+## usage
+
+This password policy provider is not ready for use.
+
+---
+Copyright 2023 Luca Filipozzi. Some rights reserved. See [LICENSE][license].
+
+[license]: https://github.com/LucaFilipozzi/keycloak-extensions/blob/main/LICENSE.md
---
+++ custom-password-policy-provider/src/main/java/com/github/lucafilipozzi/keycloak/policy/CustomPasswordPolicyProvider.java
@@ -0,0 +1,39 @@
+// Copyright 2023 Luca Filipozzi. Some rights reserved. See LICENSE.
+
+package com.github.lucafilipozzi.keycloak.policy;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.jbosslog.JBossLog;
+import org.keycloak.models.KeycloakSession;
+import org.keycloak.models.RealmModel;
+import org.keycloak.models.UserModel;
+import org.keycloak.policy.PasswordPolicyProvider;
+import org.keycloak.policy.PolicyError;
+
+@JBossLog
+@RequiredArgsConstructor
+public class CustomPasswordPolicyProvider implements PasswordPolicyProvider {
+  private final KeycloakSession session;
+
+  @Override
+  public PolicyError validate(RealmModel realm, UserModel user, String password) {
+    UserModel authenticatedUser = session.getContext().getAuthenticationSession().getAuthenticatedUser();
+    LOG.infof("realm=%s user=%s authenticatedUser=%s", realm.getName(), user.getUsername(), authenticatedUser.getUsername());
+    return null; // equivalent to 'no error'
+  }
+
+  @Override
+  public PolicyError validate(String user, String password) {
+    return null; // equivalent to 'no error'
+  }
+
+  @Override
+  public Object parseConfig(String value) {
+    return null; // TODO
+  }
+
+  @Override
+  public void close() {
+    // intentionally empty
+  }
+}
