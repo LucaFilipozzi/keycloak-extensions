0d91da6cb90304f070c7f08d354be26c09eb99fd
Luca Filipozzi <luca.filipozzi@gmail.com>
2023-09-18 19:48:37 -0700

    add custom-username-password-form authenticator

---
+++ custom-username-password-form/README.md
@@ -0,0 +1,11 @@
+# custom-username-password-form
+
+## description
+
+This custom username password form returns ACCOUNT_DISABLED rather than INVALID_USER when
+an account is disabled by the brute force detector.
+
+---
+Copyright 2023 Luca Filipozzi. Some rights reserved. See [LICENSE][license].
+
+[license]: https://github.com/LucaFilipozzi/keycloak-extensions/blob/main/LICENSE.md
---
+++ custom-username-password-form/src/main/java/com/github/lucafilipozzi/keycloak/authentication/authenticators/CustomUsernamePasswordForm.java
@@ -0,0 +1,14 @@
+// Copyright 2023 Luca Filipozzi. Some rights reserved.
+
+package com.github.lucafilipozzi.keycloak.authentication.authenticators;
+
+import org.keycloak.authentication.authenticators.browser.UsernamePasswordForm;
+import org.keycloak.services.messages.Messages;
+
+public class CustomUsernamePasswordForm extends UsernamePasswordForm {
+
+    @Override
+    protected String disabledByBruteForceError() {
+        return Messages.ACCOUNT_DISABLED;
+    }
+}
