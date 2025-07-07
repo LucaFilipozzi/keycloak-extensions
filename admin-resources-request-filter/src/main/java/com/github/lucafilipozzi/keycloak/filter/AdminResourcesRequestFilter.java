9ce9f14766c9b803ff07d7dc1e12c6a211579043
Luca Filipozzi <luca.filipozzi@gmail.com>
2025-07-07 15:15:22 -0700

    improving clarity; try MethodHandles next

--- admin-resources-request-filter/src/main/java/com/github/lucafilipozzi/keycloak/filter/AdminResourcesRequestFilter.java
+++ admin-resources-request-filter/src/main/java/com/github/lucafilipozzi/keycloak/filter/AdminResourcesRequestFilter.java
@@ -2,7 +2,8 @@
 
 package com.github.lucafilipozzi.keycloak.filter;
 
-import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.Sets;
 import jakarta.annotation.Priority;
 import jakarta.ws.rs.Priorities;
 import jakarta.ws.rs.container.ContainerRequestContext;
@@ -19,6 +20,7 @@ import java.util.Set;
 import java.util.stream.Collectors;
 import lombok.extern.jbosslog.JBossLog;
 import org.apache.commons.collections4.map.MultiKeyMap;
+import org.keycloak.component.ComponentValidationException;
 import org.keycloak.models.ClientModel;
 import org.keycloak.models.KeycloakSession;
 import org.keycloak.models.RealmModel;
@@ -37,13 +39,17 @@ import org.keycloak.services.resources.admin.UsersResource;
 @Priority(Priorities.AUTHORIZATION)
 @JBossLog
 public class AdminResourcesRequestFilter implements ContainerRequestFilter {
-  private final static String AGENCY_MANAGE_PASSWORDS = "agency-manage-passwords";
+  // users having this realm-management role assigned can only manage the credentials of other users
+  private final static String MANAGE_CREDENTIALS_ONLY = "agency-manage-passwords";
 
-  private final static String AGENCY_MANAGE_USERS = "agency-manage-users";
+  // users having this realm-management role assigned can only manage the profiles (and credentials) of other users
+  private final static String MANAGE_PROFILE_ONLY = "manage-users"; // FIXME use: "agency-manage-users"
 
+  // key is roleName, resourceClassName, resourceMethodName; value is boolean where false means deny
   private final MultiKeyMap<String, Boolean> permissions;
 
-  private final Set<String> roles;
+  // be efficient ... in filter(), below, only handle roles that have been added to the permission map
+  private final Set<String> applicableRoleNames;
 
   @Context
   private KeycloakSession session;
@@ -54,62 +60,90 @@ public class AdminResourcesRequestFilter implements ContainerRequestFilter {
   @Context
   private UriInfo uriInfo;
 
-  public AdminResourcesRequestFilter() throws NoSuchMethodException {
-    permissions = new MultiKeyMap<>();
+  public AdminResourcesRequestFilter() {
+    // note to future self - we're using getDeclaredMethod() because:
+    // (1) we want the filter() method to return quickly; it already has resourceInfo populated
+    //     with resourceClass and resourceMethod, so let's use those directly rather than trying
+    //     to parse uriInfo path ourselves or using an external dependency like casbin
+    // (2) our IDE (IntelliJ IDEA) provides excellent support for populating the method name and
+    //     method parameters for getDeclaredMethod, leaving no opportunity for typos
+    //
+    // but the use of getDeclaredMethod() makes this filter very sensitive to refactoring of the
+    // following keycloak classes:
+    //   * org.keycloak.services.resources.admin.ClientRoleMappingsResource
+    //   * org.keycloak.services.resources.admin.RealmAdminResource
+    //   * org.keycloak.services.resources.admin.RoleMapperResource
+    //   * org.keycloak.services.resources.admin.UserResource
+    //   * org.keycloak.services.resources.admin.UsersResource
+    //
+    // so let's catch any NoSuchMethodExceptions thrown during construction and throw an exception
+    // that will prevent keycloak from starting ... this should get caught during upgrade testing
+    //
+    // remember, we prefer compile-time errors over startup-time errors over run-time errors
 
-    // users
-    Method addUser = UsersResource.class.getDeclaredMethod("createUser", UserRepresentation.class);
-    Method delUser = UserResource.class.getDeclaredMethod("deleteUser");
-    Method modUser = UserResource.class.getDeclaredMethod("updateUser", UserRepresentation.class);
-    denyAccess(ImmutableList.of(AGENCY_MANAGE_PASSWORDS),
-        ImmutableList.of(addUser, delUser, modUser));
-    // admins who have agency-manage-users can add, del, and mod users
+    try {
 
-    // credentials
-    Method getCredentials = UserResource.class.getDeclaredMethod("credentials");
-    Method addCredential = UserResource.class.getDeclaredMethod("resetPassword", CredentialRepresentation.class);
-    Method delCredential = UserResource.class.getDeclaredMethod("removeCredential", String.class);
-    // admins who have agency-manage-users and agency-manage-passwords roles can reset passwords
+      permissions = new MultiKeyMap<>();
 
-    // role mappings
-    Method getRoleMappings = RoleMapperResource.class.getDeclaredMethod("getRoleMappings");
-    Method addRealmRoleMappings = RoleMapperResource.class.getDeclaredMethod("addRealmRoleMappings", List.class);
-    Method delRealmRoleMappings = RoleMapperResource.class.getDeclaredMethod("deleteRealmRoleMappings", List.class);
-    Method addClientRoleMapping = ClientRoleMappingsResource.class.getDeclaredMethod("addClientRoleMapping", List.class);
-    Method delClientRoleMapping = ClientRoleMappingsResource.class.getDeclaredMethod("deleteClientRoleMapping", List.class);
-    denyAccess(ImmutableList.of(AGENCY_MANAGE_USERS, AGENCY_MANAGE_PASSWORDS),
-        ImmutableList.of(getRoleMappings, addRealmRoleMappings, delRealmRoleMappings, addClientRoleMapping, delClientRoleMapping));
+      // users
+      Method getUsers = UsersResource.class.getDeclaredMethod("getUsers", String.class, String.class, String.class, String.class, String.class,
+          Boolean.class, String.class, String.class, Integer.class, Integer.class, Boolean.class, Boolean.class, Boolean.class, String.class);
+      Method getUser = UserResource.class.getDeclaredMethod("getUser", boolean.class);
+      Method addUser = UsersResource.class.getDeclaredMethod("createUser", UserRepresentation.class);
+      Method delUser = UserResource.class.getDeclaredMethod("deleteUser");
+      Method modUser = UserResource.class.getDeclaredMethod("updateUser", UserRepresentation.class);
+      denyAccess(ImmutableSet.of(MANAGE_CREDENTIALS_ONLY),
+          ImmutableSet.of(addUser, delUser, modUser));
 
-    // group memberships
-    Method getGroupMemberships = UserResource.class.getDeclaredMethod("groupMembership", String.class, Integer.class, Integer.class, boolean.class);
-    Method addGroupMembership = UserResource.class.getDeclaredMethod("joinGroup", String.class);
-    Method delGroupMembership = UserResource.class.getDeclaredMethod("removeMembership", String.class);
-    denyAccess("manage-users", ImmutableList.of(getGroupMemberships, addGroupMembership, delGroupMembership));
-    denyAccess(ImmutableList.of(AGENCY_MANAGE_USERS, AGENCY_MANAGE_PASSWORDS),
-        ImmutableList.of(getGroupMemberships, addGroupMembership, delGroupMembership));
+      // credentials
+      Method getCredentials = UserResource.class.getDeclaredMethod("credentials");
+      Method addCredential = UserResource.class.getDeclaredMethod("resetPassword", CredentialRepresentation.class);
+      Method delCredential = UserResource.class.getDeclaredMethod("removeCredential", String.class);
 
-    // consents
-    Method getConsents = UserResource.class.getDeclaredMethod("getConsents");
-    Method delConsent = UserResource.class.getDeclaredMethod("revokeConsent", String.class);
-    denyAccess(ImmutableList.of(AGENCY_MANAGE_USERS, AGENCY_MANAGE_PASSWORDS),
-        ImmutableList.of(getConsents, delConsent));
+      // role mappings
+      Method getRoleMappings = RoleMapperResource.class.getDeclaredMethod("getRoleMappings");
+      Method addRealmRoleMappings = RoleMapperResource.class.getDeclaredMethod("addRealmRoleMappings", List.class);
+      Method delRealmRoleMappings = RoleMapperResource.class.getDeclaredMethod("deleteRealmRoleMappings", List.class);
+      Method addClientRoleMapping = ClientRoleMappingsResource.class.getDeclaredMethod("addClientRoleMapping", List.class);
+      Method delClientRoleMapping = ClientRoleMappingsResource.class.getDeclaredMethod("deleteClientRoleMapping", List.class);
+      denyAccess(ImmutableSet.of(MANAGE_PROFILE_ONLY, MANAGE_CREDENTIALS_ONLY),
+          ImmutableSet.of(getRoleMappings, addRealmRoleMappings, delRealmRoleMappings, addClientRoleMapping, delClientRoleMapping));
 
-    // federated identities
-    Method getFederatedIdentities = UserResource.class.getDeclaredMethod("getFederatedIdentities", UserModel.class);
-    Method getFederatedIdentity = UserResource.class.getDeclaredMethod("getFederatedIdentity");
-    Method addFederatedIdentity = UserResource.class.getDeclaredMethod("addFederatedIdentity", String.class, FederatedIdentityRepresentation.class);
-    Method delFederatedIdentity = UserResource.class.getDeclaredMethod("removeFederatedIdentity", String.class);
-    denyAccess(ImmutableList.of(AGENCY_MANAGE_USERS, AGENCY_MANAGE_PASSWORDS),
-        ImmutableList.of(getFederatedIdentities, getFederatedIdentity, addFederatedIdentity, delFederatedIdentity));
+      // group memberships
+      Method getGroupMemberships = UserResource.class.getDeclaredMethod("groupMembership", String.class, Integer.class, Integer.class, boolean.class);
+      Method addGroupMembership = UserResource.class.getDeclaredMethod("joinGroup", String.class);
+      Method delGroupMembership = UserResource.class.getDeclaredMethod("removeMembership", String.class);
+      denyAccess(ImmutableSet.of(MANAGE_PROFILE_ONLY, MANAGE_CREDENTIALS_ONLY),
+          ImmutableSet.of(getGroupMemberships, addGroupMembership, delGroupMembership));
 
-    // sessions
-    Method getSessions = UserResource.class.getDeclaredMethod("getSessions");
-    Method delSessions = UserResource.class.getDeclaredMethod("logout");
-    Method delSession = RealmAdminResource.class.getDeclaredMethod("deleteSession", String.class, boolean.class);
-    denyAccess(ImmutableList.of(AGENCY_MANAGE_USERS, AGENCY_MANAGE_PASSWORDS),
-        ImmutableList.of(getSessions, delSessions, delSession));
+      // consents
+      Method getConsents = UserResource.class.getDeclaredMethod("getConsents");
+      Method delConsent = UserResource.class.getDeclaredMethod("revokeConsent", String.class);
+      denyAccess(ImmutableSet.of(MANAGE_PROFILE_ONLY, MANAGE_CREDENTIALS_ONLY),
+          ImmutableSet.of(getConsents, delConsent));
 
-    roles = permissions.keySet().stream().map(multiKey -> multiKey.getKey(0)).collect(Collectors.toSet());
+      // federated identities
+      Method getFederatedIdentities = UserResource.class.getDeclaredMethod("getFederatedIdentities", UserModel.class);
+      Method getFederatedIdentity = UserResource.class.getDeclaredMethod("getFederatedIdentity");
+      Method addFederatedIdentity = UserResource.class.getDeclaredMethod("addFederatedIdentity", String.class, FederatedIdentityRepresentation.class);
+      Method delFederatedIdentity = UserResource.class.getDeclaredMethod("removeFederatedIdentity", String.class);
+      denyAccess(ImmutableSet.of(MANAGE_PROFILE_ONLY, MANAGE_CREDENTIALS_ONLY),
+          ImmutableSet.of(getFederatedIdentities, getFederatedIdentity, addFederatedIdentity, delFederatedIdentity));
+
+      // sessions
+      Method getSessions = UserResource.class.getDeclaredMethod("getSessions");
+      Method delSessions = UserResource.class.getDeclaredMethod("logout");
+      Method delSession = RealmAdminResource.class.getDeclaredMethod("deleteSession", String.class, boolean.class);
+      denyAccess(ImmutableSet.of(MANAGE_PROFILE_ONLY, MANAGE_CREDENTIALS_ONLY),
+          ImmutableSet.of(getSessions, delSessions, delSession));
+
+      applicableRoleNames = permissions.keySet().stream().map(multiKey -> multiKey.getKey(0)).collect(Collectors.toSet());
+
+    } catch (NoSuchMethodException e) {
+
+      throw new ComponentValidationException("AdminResourceRequestFilter could not be initialized");
+
+    }
   }
 
   @Override
@@ -119,52 +153,55 @@ public class AdminResourcesRequestFilter implements ContainerRequestFilter {
     }
 
     UserModel user = session.getContext().getUser();
-    if (user == null) {
-      LOG.error("user is null"); // should never happen
+    if (user == null) { // should never happen given the path-based guard above
+      LOG.error("user is null");
       return;
     }
 
     RealmModel realm = session.getContext().getRealm();
-    if (realm == null) {
-      LOG.error("realm is null"); // should never happen
+    if (realm == null) { // should never happen given the path-based guard above
+      LOG.error("realm is null");
       return;
     }
 
-    ClientModel client = session.clients().getClientByClientId(realm, "realm-management");
-    if (client == null) {
-      LOG.error("realm-management client is null"); // should never happen
+    ClientModel realmManagementClient = session.clients().getClientByClientId(realm, "realm-management");
+    if (realmManagementClient == null) { // should never happen given the path-based guard above
+      LOG.error("realm-management client is null");
       return;
     }
 
+    // using injected resourceInfo to get the resourceClassName and resourceMethodName efficiently
     String resourceClassName = resourceInfo.getResourceClass().getName();
-
     String resourceMethodName = resourceInfo.getResourceMethod().getName();
 
     boolean permitted = user
-        .getClientRoleMappingsStream(client)
+        .getClientRoleMappingsStream(realmManagementClient)
         .map(RoleModel::getName)
-        .filter(roles::contains) // be efficient; note that allMatch() of an empty stream is true
+        .filter(applicableRoleNames::contains) // note that allMatch() of an empty stream is true, which is desired behaviour (default allow)
         .allMatch(roleName -> Optional.ofNullable(permissions.get(roleName, resourceClassName, resourceMethodName)).orElse(true));
 
-    LOG.infof("resourceClassName=%s resourceMethodName=%s permitted=%b", resourceClassName, resourceMethodName, permitted);
-
-    if (!permitted) {
+    if (permitted) {
+      // TODO change to debugf
+      LOG.infof("access granted: resourceClassName=%s resourceMethodName=%s realm=%s user=%s",
+          resourceClassName, resourceMethodName, realm.getName(), user.getUsername());
+    } else {
+      LOG.infof("access denied: resourceClassName=%s resourceMethodName=%s realm=%s user=%s",
+          resourceClassName, resourceMethodName, realm.getName(), user.getUsername());
       requestContext.abortWith(Response.status(Response.Status.FORBIDDEN).build());
     }
   }
 
-  private void denyAccess(List<String> roleNames, List<Method> resourceMethods) {
-    roleNames.forEach(roleName -> denyAccess(roleName, resourceMethods));
-  }
-
-  private void denyAccess(String roleName, List<Method> resourceMethods) {
-    resourceMethods.forEach(resourceMethod -> denyAccess(roleName, resourceMethod));
-  }
-
-  private void denyAccess(String roleName, Method resourceMethod) {
-    // add MultiKey <roleName, resourceClassName, resourceMethodName> with value set to false for
-    // operations to be DENIED to users having the specified roleName assigned; unless explicitly
-    // added, operations are DEFAULT ALLOWED: see Optional.ofNullable().orElse(true) in filter()
-    permissions.put(roleName, resourceMethod.getDeclaringClass().getName(), resourceMethod.getName(), false);
+  private void denyAccess(Set<String> roleNames, Set<Method> resourceMethods) {
+    Sets.cartesianProduct(roleNames, resourceMethods).forEach(combination -> {
+      String roleName = (String) combination.get(0);
+      Method resourceMethod = (Method) combination.get(1);
+      Class<?> resourceClass = resourceMethod.getDeclaringClass();
+      // add MultiKey <roleName, resourceClassName, resourceMethodName> with value set to false for
+      // operations to be DENIED to users having the specified roleName assigned; unless explicitly
+      // added, operations are DEFAULT ALLOWED: see Optional.ofNullable().orElse(true) in filter()
+      permissions.put(roleName, resourceClass.getName(), resourceMethod.getName(), false);
+      // note to future self - using resourceMethod.getDeclaringClass().getName() and resourceMethod.getName()
+      // for obtaining the respective names to add to the permissions map, thereby leaving no opportunity for typos
+    });
   }
 }
